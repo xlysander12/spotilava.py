07510bc5fa84ca98eecde37889d39d152520a420
Tiago <xlysander12pt@gmail.com>
2021-09-21 15:50:35 +0100

    Fixed generator problem Added option to convert the playlist using the tracks themselves instead of only the url

--- spotilavapy/spotilava.py
+++ spotilavapy/spotilava.py
@@ -25,7 +25,7 @@ class SpotiLava:
 
     def get_track(self, url):
         """
-        Regular Spotify API request (no transform)
+        Regular Spotify API request (no convertion applied)
         :param url:
         :return:
         """
@@ -41,8 +41,8 @@ class SpotiLava:
     def get_playlist_tracks(self, url):
         """
         Regular Spotify API request (no transform)
-        :param url:
-        :return:
+        :param url: URL, URI or STR_URI of the playlist
+        :return: Spotify tracks from the playlist
         """
         try:
             tracks = self._spotify.playlist_items(url, limit=self._limit)
@@ -57,26 +57,35 @@ class SpotiLava:
         """
         Convert a spotify track to a lavalink one so it can be played by it
         :param url: URL, URI or STR_URI of the track
-        :return list with all tracks:
+        :return: The first item of the search query from YouTube
         """
         track = self.get_track(url)
         result = await self._player.node.get_tracks(f"ytsearch:{', '.join(track['artists'][i]['name'] for i in range(len(track['artists'])))} - {track['name']}")
         return result["tracks"][0]
 
-    async def convert_playlist(self, url):
+    async def convert_playlist(self, *, url:str=None, spotify_tracks:list=None):
         """
         Convert a spotify playlist to a list of lavalink tracks so they can be played by it
-        :param url: URL, URI or STR_URI of the track
-        :return:
+        :param url: URL, URI or STR_URI of the track; This kwarg has priority over `tracks`
+        :param spotify_tracks: List of Spotify tracks to convert
+        :return: A list of lavalink tracks to directly load to your player
         """
-        items = self.get_playlist_tracks(url)
         tracks = []
-        for item in items["items"]:
-            tracks.append(await self.convert_track(item["track"]["uri"]))
+        if url is not None:
+            items = self.get_playlist_tracks(url)
+            for item in items["items"]:
+                tracks.append(await self.convert_track(item["track"]["uri"]))
+        elif spotify_tracks is not None:
+            for track in spotify_tracks:
+                tracks.append(await self.convert_track(track["track"]["uri"]))
 
         return tracks
 
-    async def convert_playlist_generator(self, url):
-        items = self.get_playlist_tracks(url)
-        async for item in items["items"]:
-            yield await self.convert_track(item["track"]["uri"])
+    async def convert_playlist_generator(self,* , url:str=None, spotify_tracks:list=None):
+        if url is not None:
+            items = self.get_playlist_tracks(url)
+            for item in items["items"]:
+                yield await self.convert_track(item["track"]["uri"])
+        elif spotify_tracks is not None:
+            for track in spotify_tracks:
+                yield await self.convert_track(track["track"]["uri"])
